9dbf71c4c762605d53048766ae619bb70d2ebb8c
Edison Pizarro Godoy <edipizarro@uc.cl>
2022-08-12 14:37:36 -0400

    feature/ Added top_retweeted tweets

--- features/__init__.py
+++ features/__init__.py
@@ -1,6 +1,9 @@
 # Import features
 
-## Import top tweets feature
+## Import top_retweeted feature
+from features.top_retweeted import top_retweeted
+top_retweeted = top_retweeted
+
 ## Import top users feature
 ## Import top days feature
 ## Import top hashtags feature
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Python modules
 import json
+from tkinter.tix import TCL_WINDOW_EVENTS
 
 # PIP modules
 
@@ -13,7 +14,13 @@ with open(filename) as file_object:
 if __name__ == '__main__':
     # Use features
 
-    ## Use top tweets feature
+    ## Use top retweeted feature
+    top_retweeted = features.top_retweeted(dataset, n=10)
+    print("Top retweeted:")
+    for tweet in top_retweeted:
+        print(tweet['retweetCount'])
+    print("\n")
+
     ## Use top users feature
     ## Use top days feature
     ## Use top hashtags feature
